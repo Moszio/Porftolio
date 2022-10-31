0f8be9de7d8a88597a9234bfabd97ee297b543fc
andor <latandor@gmail.com>
2022-10-31 10:02:14 -0400

    Projects component

---
+++ components/Projects.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import porjectStyles from '../styles/Project.module.css'
+
+const Projects = () => {
+  return <div className={porjectStyles.container}>Object</div>
+}
+
+export default Projects
--- pages/project.js
+++ pages/project.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import Projects from '../components/Projects'
 
 const project = () => {
-  return <div>project</div>
+  return (
+    <div>
+      project
+      <Projects />
+    </div>
+  )
 }
 
 export default project
